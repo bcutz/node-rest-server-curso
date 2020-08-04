d52bbc6a9aa92bfe387546b1368373430a186b3f
bcutz <bcutzs@miumg.edu.gt>
2020-08-03 23:21:24 -0600

    Login Normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -1,17 +1,33 @@
-//=============
+//================================
 //puerto
-//=============
+//================================
 process.env.PORT = process.env.PORT || 3001;
 
-//=============
+//================================
 //Enterno
-//=============
+//================================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 
-//=============
+//=================================
+// Vencimiento del Token
+//=================================
+// 60 segundos
+// 60 minutos
+// 24 horas
+//30 Dias
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+
+//=================================
+// Seed del Token
+//=================================
+process.env.SEED = process.env.SEED || 'este-es-el-seed-dev';
+
+
+//=================================
 // Base de datos
-//=============
+//=================================
 let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
